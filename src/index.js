008d6b5941ac689df1e0396a713df9ca133ade72
Kate-Shepel <kateborichevskaya@gmail.com>
2024-07-05 17:01:45 +0200

    refactor: add Provider to start connection between redux and react

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { legacy_createStore as createStore, bindActionCreators } from 'redux';
+import { legacy_createStore as createStore} from 'redux';
 import reducer from './reducer';
-import * as actions from './actions';
-import Counter from './Counter';
+import App from './components/App';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
-const {dispatch, subscribe, getState} = store;
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-const update = () => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <Counter
-        counter={getState().value}
-        inc={inc}
-        dec={dec}
-        rnd={() => {
-          const value = Math.floor(Math.random() * 10);
-          rnd(value);
-        }}
-      />
-    </React.StrictMode>
-  );
-}
-update();
-subscribe(update);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  {/* передаём store по всей иерархии нашего дерева компонентов */}
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </React.StrictMode>
+);
