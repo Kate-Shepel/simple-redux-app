c7f0fade3275535097764ac8b1345e8b15c490fc
Kate-Shepel <kateborichevskaya@gmail.com>
2024-07-05 16:44:35 +0200

    feat: adding react approach to the View

--- src/index.js
+++ src/index.js
@@ -3,36 +3,27 @@ import ReactDOM from 'react-dom/client';
 import { legacy_createStore as createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
 import * as actions from './actions';
+import Counter from './Counter';
 
 const store = createStore(reducer);
 const {dispatch, subscribe, getState} = store;
-
-const update = () => {
-    document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// }
-
 const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-
-document.getElementById('rnd').addEventListener('click', () => {
-    const value = Math.floor(Math.random() * 10);
-    rnd(value);
-});
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <>
-
-    </>
-  </React.StrictMode>
-);
+const update = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <Counter
+        counter={getState().value}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+          const value = Math.floor(Math.random() * 10);
+          rnd(value);
+        }}
+      />
+    </React.StrictMode>
+  );
+}
+update();
+subscribe(update);
