9c8575bb8c8ee4f6a4491f1070105c4e5bedf124
Kate-Shepel <kateborichevskaya@gmail.com>
2024-07-06 15:08:24 +0200

    refactor: connect func can work with class component as well

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,20 @@
+import { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-//import { bindActionCreators } from 'redux';
 
-const Counter = ({counter, inc, dec, rnd}) => {
+class Counter extends Component {
 
-  return (
-    <div className="jumbotron">
-      <h1>{counter}</h1>
-      <button onClick={dec} className="btn btn-primary">DEC</button>
-      <button onClick={inc} className="btn btn-primary">INC</button>
-      <button onClick={rnd} className="btn btn-primary">RND</button>
-    </div>
-  )
+  render() {
+    const {counter, inc, dec, rnd} = this.props;
+    return (
+      <div className="jumbotron">
+        <h1>{counter}</h1>
+        <button onClick={dec} className="btn btn-primary">DEC</button>
+        <button onClick={inc} className="btn btn-primary">INC</button>
+        <button onClick={rnd} className="btn btn-primary">RND</button>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
@@ -20,17 +23,5 @@ const mapStateToProps = (state) => {
     counter: state.value
   }
 }
-
-// const mapDispatchToProps = (dispatch) => {
-//   const {inc, dec, rnd} = bindActionCreators(actions, dispatch)
-
-//   return {
-//     inc, //inc: inc
-//     dec,
-//     rnd
-//   }
-
-//   // return bindActionCreators(actions, dispatch) more short version instead of the content on the top
-// }
 
 export default connect(mapStateToProps, actions)(Counter);
