d2dd4df876bc2320f6efcbfcfecbbf7fe747cc91
Kate-Shepel <kateborichevskaya@gmail.com>
2024-07-06 14:59:37 +0200

    refactor: finally connect react and redux (1st way with connect)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,9 @@
 import { connect } from 'react-redux';
-import { inc, dec, rnd } from '../actions';
+import * as actions from '../actions';
+//import { bindActionCreators } from 'redux';
 
 const Counter = ({counter, inc, dec, rnd}) => {
+
   return (
     <div className="jumbotron">
       <h1>{counter}</h1>
@@ -13,19 +15,22 @@ const Counter = ({counter, inc, dec, rnd}) => {
 }
 
 const mapStateToProps = (state) => {
+
   return {
     counter: state.value
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    inc: () => dispatch(inc()),
-    dec: () => dispatch(dec()),
-    rnd: () => {
-      const value = Math.floor(Math.random() * 10);
-      dispatch(rnd(value))
-    }
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+// const mapDispatchToProps = (dispatch) => {
+//   const {inc, dec, rnd} = bindActionCreators(actions, dispatch)
+
+//   return {
+//     inc, //inc: inc
+//     dec,
+//     rnd
+//   }
+
+//   // return bindActionCreators(actions, dispatch) more short version instead of the content on the top
+// }
+
+export default connect(mapStateToProps, actions)(Counter);
